58f1dc0608299724e40de7d8888fcf5a24406ac3
Matt Jones <gitcode@magisa.org>
2021-09-28 18:27:22 -0800

    Updated tab query to select current window, and add permissions.

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 function openPage() {
-  browser.tabs.query({active: true})
+  browser.tabs.query({active: true, currentWindow: true})
     .then(tabs => browser.tabs.get(tabs[0].id))
     .then(tab => { 
         console.info("TabID: " + tab.id); 
         console.info("Title: " + tab.title);
         browser.tabs.executeScript(tab.id, {
-            code: `json = window.document.querySelector('script[type="application/ld+json"]'); browser.runtime.sendMessage(json.innerHTML)`
+            code: `json = window.document.querySelector('script[type="application/ld+json"]'); if (json != null) {browser.runtime.sendMessage(json.innerHTML)} else {alert("Whoa, no JSON-LD here!")};`
         })
     });
 
@@ -16,7 +16,7 @@ function jsonListener(json) {
     playground_uri = "https://json-ld.org/playground/#startTab=tab-expanded&json-ld=" + encoded
     console.info(playground_uri)
     browser.tabs.create({
-      url: playground_uri
+        url: playground_uri
     });
 }
 
